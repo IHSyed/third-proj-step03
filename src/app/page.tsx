d02d4df5094566d7520641187c398bd6e52da8f3
Iqbal Hussain Syed <65956129+IHSyed@users.noreply.github.com>
2023-03-04 16:17:15 +0500

    First Commit

---
+++ src/app/page.tsx
@@ -0,0 +1,25 @@
+import Image from 'next/image'
+
+export default function () {
+  return (
+    <div className = "flex">
+      <div className='quote'>
+        <p>"The success combimation in business is - DO WHAT YOU DO BETTER,  and DO MORE WHAT YOU DO"</p>
+        <span>- David J. Shwartz</span>
+      </div>
+
+      <div className='quote'>
+        <p>"Give out what you want to come back"</p>
+        <span>- Robin Sharma</span>
+      </div>
+
+      <div className='quote'>
+        <p>"WORK, WORK AND WORK"</p>
+        <span>- Quaid E Azam MA. Jinnah</span>
+      </div>
+    </div>
+  )
+}
+
+
+
